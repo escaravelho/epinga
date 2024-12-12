bb49429c8bd4e231b12917dea29256f1cee568b4
esoft46 - Wallace Vieira <ex183142@g.unicamp.br>
2024-12-12 15:20:32 -0300

    Update models.py

--- epinga/models.py
+++ epinga/models.py
@@ -1,11 +1,13 @@
+from datetime import datetime, timedelta
 from typing import Annotated, Optional
 
-from fastapi import Depends, FastAPI, HTTPException, Query
-from sqlmodel import Field, Session, SQLModel, create_engine, select, Relationship
-from datetime import datetime, timedelta
+from sqlmodel import Field, Relationship, SQLModel
+
+
 class Plan(SQLModel, table=True):
     id: int = Field(primary_key=True)
     name: str
+
 
 class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
@@ -16,10 +18,13 @@ class User(SQLModel, table=True):
     plan: Plan = Relationship()
     api_keys: Optional[list["APIKey"]] = Relationship(back_populates="user")
 
+
 class APIKey(SQLModel, table=True):
     id: int = Field(primary_key=True)
     key: str
     user_id: int = Field(foreign_key="user.id")
     user: "User" = Relationship(back_populates="api_keys")
     # Defult to one month from now
-    expiration_date: datetime = Field(default_factory=lambda: datetime.now() + timedelta(days=30))
+    expiration_date: datetime = Field(
+        default_factory=lambda: datetime.now() + timedelta(days=30)
+    )
